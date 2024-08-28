474e53c3b5e42ee1a7a5936425443df0940911d7
LCostaF <146568540+LCostaF@users.noreply.github.com>
2024-08-28 11:02:10 -0300

    refactor: Generalize functions, reorganize code

--- src/sequential_solver.py
+++ src/sequential_solver.py
@@ -1,46 +1,6 @@
 import time
-from save_board import seq_print_board, seq_print_summary
-
-def is_position_safe(board, row, col):
-    # Verifica se é seguro colocar uma rainha na posição (row, col) do tabuleiro.
-
-    # Verifica a linha à esquerda
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
-
-    # Verifica a diagonal superior esquerda
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    # Verifica a diagonal inferior esquerda
-    for i, j in zip(range(row, len(board)), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    return True
-
-def solve_nqueens(board, col, solutions):
-    # Resolve o problema das N-rainhas usando backtracking e armazena todas as soluções encontradas.
-    if col >= len(board):
-        # Encontrou uma solução, adicione uma cópia do tabuleiro à lista de soluções
-        solutions.append([row[:] for row in board])
-        return
-
-    for row in range(len(board)):
-        if is_position_safe(board, row, col):
-            board[row][col] = 1
-            # print(f"Posicionando rainha em ({row}, {col}).")
-
-            solve_nqueens(board, col + 1, solutions)
-            board[row][col] = 0  # Backtracking
-            # print(f"Removendo rainha de ({row}, {col}) devido a conflito.")
-
-def print_boards(solutions):
-    # Imprime todas as soluções encontradas.
-    for index, board in enumerate(solutions):
-        seq_print_board(board, index + 1)  # Passa o número da solução para a função
+from save_board import print_board, print_summary, seq_logger
+from solve_utils import solve_nqueens, print_boards
 
 def main(n):
     # Função principal para resolver o problema das N-rainhas e imprimir todas as soluções.
@@ -56,12 +16,12 @@ def main(n):
     print(f"Total de soluções encontradas: {len(solutions)}")
     print(f"Tempo total de execução: {total_time:.2f} segundos\n")
     
-    # Imprimir soluções usando a função sequencial
-    print_boards(solutions)
+    # Imprimir soluções usando a função genérica
+    print_boards(solutions, lambda board, index: print_board(board, index, seq_logger))
 
     # Imprime o resumo das soluções
-    seq_print_summary(len(solutions), total_time)
+    print_summary(len(solutions), total_time, seq_logger)
 
 # Exemplo de uso:
 if __name__ == "__main__":
-    main(13)  # Substitua 8 pelo tamanho desejado do tabuleiro
+    main(8)  # Substitua 8 pelo tamanho desejado do tabuleiro
--- src/thread_solver.py
+++ src/thread_solver.py
@@ -1,42 +1,7 @@
 import time
 from threading import Thread, Lock
-from save_board import thr_print_board, thr_print_summary
-
-def is_position_safe(board, row, col):
-    # Verifica se é seguro colocar uma rainha na posição (row, col) do tabuleiro.
-
-    # Verifica a linha à esquerda
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
-
-    # Verifica a diagonal superior esquerda
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    # Verifica a diagonal inferior esquerda
-    for i, j in zip(range(row, len(board)), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    return True
-
-def solve_nqueens(board, col, solutions):
-    # Resolve o problema das N-rainhas usando backtracking e armazena todas as soluções encontradas.
-    if col >= len(board):
-        # Encontrou uma solução, adicione uma cópia do tabuleiro à lista de soluções
-        solutions.append([row[:] for row in board])
-        return
-
-    for row in range(len(board)):
-        if is_position_safe(board, row, col):
-            board[row][col] = 1
-            # print(f"Posicionando rainha em ({row}, {col}).")
-            
-            solve_nqueens(board, col + 1, solutions)
-            board[row][col] = 0  # Backtracking
-            # print(f"Removendo rainha de ({row}, {col}) devido a conflito.")
+from save_board import print_board, print_summary, thr_logger
+from solve_utils import is_position_safe, solve_nqueens, print_boards
 
 def solve_nqueens_thread(row, n, shared_solutions, lock):
     board = [[0] * n for _ in range(n)]
@@ -46,11 +11,6 @@ def solve_nqueens_thread(row, n, shared_solutions, lock):
         solve_nqueens(board, 1, local_solutions)  # Começa da segunda coluna
         with lock:  # Bloquear a lista compartilhada para acesso seguro
             shared_solutions.extend(local_solutions)
-
-def print_boards(solutions):
-    # Imprime todas as soluções encontradas.
-    for index, board in enumerate(solutions):
-        thr_print_board(board, index + 1)  # Passa o número da solução para a função
 
 def main(n):
     # Função principal para resolver o problema das N-rainhas e imprimir todas as soluções.
@@ -77,11 +37,12 @@ def main(n):
     print(f"Total de soluções encontradas: {len(shared_solutions)}")
     print(f"Tempo total de execução: {total_time:.2f} segundos\n")
 
-    # Imprimir soluções usando a função paralela com threads
-    print_boards(shared_solutions)
+    # Imprimir soluções usando a função genérica
+    print_boards(shared_solutions, lambda board, index: print_board(board, index, thr_logger))
 
-    thr_print_summary(len(shared_solutions), total_time)
+    # Imprime o resumo das soluções
+    print_summary(len(shared_solutions), total_time, thr_logger)
 
 # Exemplo de uso:
 if __name__ == "__main__":
-    main(14) # Substitua 8 pelo tamanho desejado do tabuleiro
+    main(8) # Substitua 8 pelo tamanho desejado do tabuleiro
