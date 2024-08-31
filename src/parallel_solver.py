bb8948087c790245b48f0e8ec491f91a370c95c8
hebertwilly <hebertwilly@hotmail.com>
2024-08-31 14:49:05 -0300

    feat: adicionando o monitoramento de consumo de memória na execução de cada solução

--- src/parallel_solver.py
+++ src/parallel_solver.py
@@ -1,4 +1,5 @@
 import time
+import psutil
 from multiprocessing import Process, Manager
 from save_board import print_board, print_summary, par_logger
 from solve_utils import is_position_safe, solve_nqueens, print_boards
@@ -8,41 +9,38 @@ def solve_nqueens_parallel(row, n, shared_solutions):
     if is_position_safe(board, row, 0):
         board[row][0] = 1
         local_solutions = []
-        solve_nqueens(board, 1, local_solutions)  # Começa da segunda coluna
-        shared_solutions.extend(local_solutions)  # Adiciona as soluções encontradas ao resultado compartilhado
+        solve_nqueens(board, 1, local_solutions)
+        shared_solutions.extend(local_solutions)
 
 def main(n):
-    # Função principal para resolver o problema das N-rainhas e imprimir todas as soluções.
-    start_time = time.time() # Início da medição do tempo
-    
-    # Usando Manager para criar uma lista compartilhada entre processos
+    start_time = time.time()  # Início da medição do tempo
+    start_memory = psutil.Process().memory_info().rss  # Memória inicial
+
     with Manager() as manager:
         shared_solutions = manager.list()
         processos = []
 
-        # Criar um processo para cada linha inicial da primeira coluna
         for row in range(n):
             p = Process(target=solve_nqueens_parallel, args=(row, n, shared_solutions))
             processos.append(p)
             p.start()
 
-        # Esperar que todos os processos terminem
         for p in processos:
             p.join()
 
-        end_time = time.time() # Fim da medição do tempo
+        end_time = time.time()  # Fim da medição do tempo
+        end_memory = psutil.Process().memory_info().rss  # Memória final
 
-        total_time = end_time - start_time # Tempo total de execução
+        total_time = end_time - start_time  # Tempo total de execução
+        memory_used = (end_memory - start_memory) / (1024 ** 2)  # Memória utilizada em MB
 
         print(f"Total de soluções encontradas: {len(shared_solutions)}")
-        print(f"Tempo total de execução: {total_time:.2f} segundos\n")
-        
-        # Imprimir soluções usando a função genérica
-        print_boards(shared_solutions, lambda board, index: print_board(board, index, par_logger))
+        print(f"Tempo total de execução: {total_time:.2f} segundos")
+        print(f"Memória utilizada: {memory_used:.2f} MB\n")
 
-        # Imprime o resumo das soluções
+        print_boards(shared_solutions, lambda board, index: print_board(board, index, par_logger))
         print_summary(len(shared_solutions), total_time, par_logger)
 
 # Exemplo de uso:
 if __name__ == "__main__":
-    main(8) # Substitua 8 pelo tamanho desejado do tabuleiro
+    main(12)  # Substitua 8 pelo tamanho desejado do tabuleiro
--- src/sequential_solver.py
+++ src/sequential_solver.py
@@ -1,20 +1,26 @@
 import time
+import psutil
 from save_board import print_board, print_summary, seq_logger
 from solve_utils import solve_nqueens, print_boards
 
 def main(n):
-    # Função principal para resolver o problema das N-rainhas e imprimir todas as soluções.
     board = [[0] * n for _ in range(n)]
     solutions = []
 
     start_time = time.time()  # Início da medição do tempo
+    start_memory = psutil.Process().memory_info().rss  # Memória inicial
+
     solve_nqueens(board, 0, solutions)
+
     end_time = time.time()  # Fim da medição do tempo
+    end_memory = psutil.Process().memory_info().rss  # Memória final
 
     total_time = end_time - start_time  # Tempo total de execução
+    memory_used = (end_memory - start_memory) / (1024 ** 2)  # Memória utilizada em MB
 
     print(f"Total de soluções encontradas: {len(solutions)}")
-    print(f"Tempo total de execução: {total_time:.2f} segundos\n")
+    print(f"Tempo total de execução: {total_time:.2f} segundos")
+    print(f"Memória utilizada: {memory_used:.2f} MB\n")
     
     # Imprimir soluções usando a função genérica
     print_boards(solutions, lambda board, index: print_board(board, index, seq_logger))
